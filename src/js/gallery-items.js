0304c55ad11bd5d42caefe504b36362bec5bc875
Roman Isko <roman_isko@ukr.net>
2025-04-30 17:53:41 +0300

    add modified files

---
+++ src/js/gallery-items.js
@@ -0,0 +1,17 @@
+export const galleryItems = [
+  {
+    preview: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+    description: 'Beautiful Tree in Field',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2016/02/19/11/23/iceberg-1209494__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2016/02/19/11/23/iceberg-1209494_1280.jpg',
+    description: 'Floating Iceberg',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2015/03/26/09/41/landscape-690588__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2015/03/26/09/41/landscape-690588_1280.jpg',
+    description: 'Mountain Landscape',
+  },
+];
